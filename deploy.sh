116b16952912bfedabca6e4bef395aed43bf8952
odetv <abdiutama02@gmail.com>
2024-06-16 17:22:16 +0800

    chore: Update deploy script to use python3 for running the API server

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ git pull origin main
 echo "Sedang berada di direktori: $(pwd)"
 
 # Hentikan aplikasi yang sedang berjalan
-PIDS=$(ps aux | grep "python api.py" | grep -v grep | awk '{print $2}')
+PIDS=$(ps aux | grep "python3 api.py" | grep -v grep | awk '{print $2}')
 if [ -n "$PIDS" ]; then
   echo "Menghentikan aplikasi dengan PID: $PIDS"
   kill -9 $PIDS
@@ -21,10 +21,7 @@ else
   echo "Tidak ada aplikasi yang berjalan."
 fi
 
-# Kembali ke direktori project untuk memastikan
-cd $PROJECT_DIR
-
-# Jalankan kembali aplikasi dengan nohup
-nohup python api.py > output.log 2>&1 &
+# Jalankan kembali aplikasi dengan nohup menggunakan python3
+nohup python3 api.py > output.log 2>&1 &
 
 echo "Deploy selesai. Aplikasi FastAPI sudah berjalan."
