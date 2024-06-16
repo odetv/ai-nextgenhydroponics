0c4c5851a043db2b120f530ab351f29053900f03
odetv <abdiutama02@gmail.com>
2024-06-16 17:26:28 +0800

    chore: Update deploy script to use uvicorn for running the API server

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ git pull origin main
 echo "Sedang berada di direktori: $(pwd)"
 
 # Hentikan aplikasi yang sedang berjalan
-PIDS=$(ps aux | grep "python api.py" | grep -v grep | awk '{print $2}')
+PIDS=$(ps aux | grep "uvicorn api:app" | grep -v grep | awk '{print $2}')
 if [ -n "$PIDS" ]; then
   echo "Menghentikan aplikasi dengan PID: $PIDS"
   kill -9 $PIDS
@@ -22,7 +22,6 @@ else
 fi
 
 # Jalankan kembali aplikasi dengan nohup menggunakan python3
-pip install -r requirements.txt
-nohup python api.py > output.log 2>&1 &
+nohup uvicorn api:app > output.log 2>&1 &
 
 echo "Deploy selesai. Aplikasi FastAPI sudah berjalan."
