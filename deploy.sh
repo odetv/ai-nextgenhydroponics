e08c6ecc65c4394531ad3a56a231b01c48a53c47
odetv <abdiutama02@gmail.com>
2024-06-16 17:02:58 +0800

    chore: Update deploy script to use correct file name and start API server

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,10 @@
-#!/bin/bash
-
-# Direktori project
 PROJECT_DIR=~/project/api-model-nextgenhydroponics
 
-# Navigasi ke direktori project
 cd $PROJECT_DIR
 
-# Tarik perubahan terbaru dari GitHub
 git pull origin main
 
-# Hentikan aplikasi yang sedang berjalan
-PIDS=$(ps aux | grep "python index.py" | grep -v grep | awk '{print $2}')
+PIDS=$(ps aux | grep "python api.py" | grep -v grep | awk '{print $2}')
 if [ -n "$PIDS" ]; then
   echo "Menghentikan aplikasi dengan PID: $PIDS"
   kill -9 $PIDS
@@ -18,7 +12,6 @@ else
   echo "Tidak ada aplikasi yang berjalan."
 fi
 
-# Jalankan kembali aplikasi
-nohup python index.py > output.log 2>&1 &
+nohup python api.py > output.log 2>&1 &
 
 echo "Deploy selesai."
